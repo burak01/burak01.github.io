4e22c54a5c5fb96634730dd00660add408a8ae20
Burak <buraksoy42@gmail.com>
2023-06-30 13:58:10 +0300

    made the app

--- genshin-ar-calculator/Site/index.js
+++ genshin-ar-calculator/Site/index.js
@@ -2,52 +2,36 @@ window.onload = function(){
 let num = Math.round(Math.random() * 9) + 1 
 
 const checkButton = document.getElementById("check")
-const num1 = document.getElementById("num1")
-const num2 = document.getElementById("num2")
-const fromButton = document.getElementById("from")
-const toButton = document.getElementById("to")
+const currentAr = document.getElementById("ar1")
+const desiredAr = document.getElementById("ar2")
+const currentXp = document.getElementById("xp1")
+const desiredXp = document.getElementById("xp2")
+const dailyXp = document.getElementById("daily")
+
 const resultText = document.getElementById("result")
-const dateInfo = document.getElementById("date")
 
-var today = new Date();
-var dstStart = new Date(today.getFullYear(), 2, 14); // Second Sunday in March
-var dstEnd = new Date(today.getFullYear(), 10, 7); // First Sunday in November
-var isDST = today >= dstStart && today < dstEnd;
-
-var offsetPT = (isDST ? 7 : 8);
-var offsetET = (isDST ? -4 : -5);
-
-const timeZones = new Map([
-   ["gmt", 0],
-   ["utc", 0],
-   ["pst", -8],
-   ["pdt", -7],
-   ["pt", offsetPT],
-   ["cst", -6],
-   ["cdt", -5],
-   ["est", -5],
-   ["edt", -4],
-   ["et", offsetET]
-
-]);
+const data = [
+   0, 375, 875, 1500, 2225, 3075, 4025, 5100, 6300, 7600, 9025, 10550, 12200, 13975, 15850, 17850,
+   20225, 22725, 25350, 28125, 30950, 34375, 38100, 42100, 46400, 50975, 55850, 61000, 66450, 72175,
+   78200, 84500, 91100, 98000, 105175, 112650, 120400, 128450, 136775, 145400, 155950, 167475,
+   179950, 193400, 207800, 223150, 239475, 256750, 275000, 294200, 320600, 349400, 380600, 414200,
+   450200, 682550, 941500, 1227250, 1540075, 1880200
+ ];
 
 function clickedCheck() {
-   let a = timeZones.get(fromButton.value.toLowerCase());
-   let b = timeZones.get(toButton.value.toLowerCase());
-   let plus1 = parseInt(num1.value) || 0;
-   let plus2 = parseInt(num2.value) || 0;   
+   var currentArValue = parseInt(currentAr.value)||0;
+   var desiredArValue = parseInt(desiredAr.value)||0;
+   var currentXpValue = parseInt(currentXp.value)||0;
+   var desiredXpValue = parseInt(desiredXp.value)||0;
+   var dailyXpValue = parseInt(dailyXp.value)||0;
 
-   let offset = (-a - plus1) + (b + plus2);
+   if (currentArValue <= 0 || desiredArValue <= 0 || data[currentArValue] + currentXpValue > data[desiredArValue] + desiredXpValue || currentArValue >= 60 || desiredArValue > 60) {
+      resultText.innerHTML = "Please enter valid arguments";
+    } else {
+      var result = Math.ceil(((data[desiredArValue] + desiredXpValue) - (data[currentArValue] + currentXpValue)) / dailyXpValue);
+      resultText.innerHTML = `If you gain ${dailyXpValue}xp every day, you will reach your goal in ${result} days.`;
+    }
 
-   let selectedDateTime = new Date(dateInfo.value);
-   if (isNaN(selectedDateTime) || selectedDateTime === null || selectedDateTime === undefined){
-      resultText.innerText = -offset>=0 ? `${fromButton.value}+${plus1} is ahead of ${toButton.value}+${plus2} by ${-offset} hour(s)` : `${fromButton.value}+${plus1} is behind ${toButton.value}+${plus2} by ${offset} hour(s)`
-
-   }else{
-      selectedDateTime.setHours(selectedDateTime.getHours() + offset);
-      resultText.innerText = "This will be " + selectedDateTime.toLocaleString();
-   }
-   
  }
 
  checkButton.onclick = clickedCheck;
